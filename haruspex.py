1c9b3f0f983a6e0ceff213a9e927213f706752c2
John Howe <johnhowe@gmail.com>
2016-01-21 22:49:26 +1300

    Interpolating

--- haruspex.py
+++ haruspex.py
@@ -39,8 +39,12 @@ def main():
 #    print "kpa axis"
 #    print kpaAxis
 
-    egoTable = egoFromLog(args.logfile[0], kpaAxis, rpmAxis)
+    egoTable = egoFromLog(args.logfile[0], kpaAxis, rpmAxis, veTable)
     print egoTable
+    print rpmAxis
+    print kpaAxis
+    print veTable
+
 
     #data log sample rate
     #friction in seconds
@@ -71,38 +75,67 @@ def isLast(itr):
         old = new
     yield True, old
 
-def egoFromLog(file, kpaAxis, rpmAxis):
+def lookup3dTable(xAxis, yAxis, table, xIndex, yIndex):
+    x0 = next((j for j in reversed(xAxis) if j <= xIndex), xAxis[0])
+    x1  = next((j for j in xAxis if j >= xIndex), xAxis[-1])
+    y0 = next(( j for j in reversed(yAxis) if j <= yIndex), yAxis[0])
+    y1  = next((j for j in yAxis if j >= yIndex), yAxis[-1])
+
+    if (x0 == x1) and (y0 == y1):
+        val = table[yAxis.index(y0), xAxis.index(x0)]
+    elif (x0 == x1):
+        val = table[yAxis.index(y0), xAxis.index(x0)] + (table[yAxis.index(y0), xAxis.index(x0)] \
+                - table[yAxis.index(y1), xAxis.index(x0)])*((yIndex-y0)/(y1-y0))
+    elif (y0 == y1):
+        val = table[yAxis.index(y0), xAxis.index(x0)] + (table[yAxis.index(y0), xAxis.index(x0)] \
+                - table[yAxis.index(y0), xAxis.index(x1)])*((xIndex-x0)/(x1-x0))
+    else:
+        area_00 = (xIndex - x0) * (yIndex - y0)
+        area_01 = (xIndex - x0) * (y1 - yIndex)
+        area_10 = (x1 - xIndex) * (yIndex - y0)
+        area_11 = (x1 - xIndex) * (y1 - yIndex)
+        area = area_00 + area_01 + area_10 + area_11
+        val = area_00 * table[yAxis.index(y1), xAxis.index(x1)] / area \
+            + area_01 * table[yAxis.index(y0), xAxis.index(x1)] / area \
+            + area_10 * table[yAxis.index(y1), xAxis.index(x0)] / area \
+            + area_11 * table[yAxis.index(y0), xAxis.index(x0)] / area
+
+    print xIndex, '\t', yIndex, '\t', x0, '\t', x1,'\t', y0, '\t', y1, '\t', val
+
+def egoFromLog(file, kpaAxis, rpmAxis, veTable):
     egoTable = np.zeros([len(kpaAxis), len(rpmAxis)])
+    xx, yy = np.meshgrid(rpmAxis, kpaAxis)
     with open(file) as csvfile:
         dialect = csv.Sniffer().sniff(csvfile.read(1024))
         csvfile.seek(0)
         reader = csv.reader(csvfile, dialect, delimiter=',')
 
         row = reader.next()
-        indexCHT = row.index("CHT")
-        indexEGO = row.index("EGO")
+        #indexCHT = row.index("CHT")
+        #indexEGO = row.index("EGO")
         indexMAP = row.index("MAP")
         indexRPM = row.index("RPM")
-        indexTPS = row.index("TPS")
-        indexETE = row.index("ETE")
+        #indexTPS = row.index("TPS")
+        #indexETE = row.index("ETE")
 
-        lastTPS = 0
-        datapoints = 0; ignoredEdge = 0; ignoredEGO = 0; ignoredETE = 0; ignoredDTPS=0;
+        #lastTPS = 0
+        datapoints = 0;
+        #ignoredEdge = 0; ignoredEGO = 0; ignoredETE = 0; ignoredDTPS=0;
         for (isLastDataPoint, row) in isLast(reader):
             datapoints += 1
 
-            CHT  = float(row[indexCHT])
-            EGO  = float(row[indexEGO])
-            ETE  = float(row[indexETE])
+            #CHT  = float(row[indexCHT])
+            #EGO  = float(row[indexEGO])
+            #ETE  = float(row[indexETE])
             MAP  = float(row[indexMAP])
             RPM  = float(row[indexRPM])
-            TPS  = float(row[indexTPS])
-            DTPS = TPS - lastTPS
-            lastTPS = TPS
+            #TPS  = float(row[indexTPS])
+            #DTPS = TPS - lastTPS
+            #lastTPS = TPS
 
-            rpmCell = [j for j in rpmAxis if j >= RPM]
-            kpaCell = [j for j in kpaAxis if j >= MAP]
-            print rpmCell, kpaCell
+            #TODO why am I looking this up? should't I just be interested in the weighting of each cell?
+            veValue = lookup3dTable(rpmAxis, kpaAxis, veTable, RPM, MAP)
+
 
     return egoTable
 
